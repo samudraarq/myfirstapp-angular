1723997829c4981096f172e6938e6804bfd3b060
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-15 21:02:59 +0700

    add success and warning components

---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warningalert',
+  templateUrl: './warningAlert.component.html',
+  styleUrls: ['./warningAlert.component.css'],
+})
+export class WarningAlertComponent {}
